2abbf7279a47446dfb3869b90505bb1e10131b1a
Sid <koolspy.siddhu@gmail.com>
2015-01-11 16:58:31 +0530

    Delete stripe.rb

--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_kqnIs2hgJxMPfAb89tGIOTg6',
-  :secret_key      => 'sk_test_c9YlfaiT0uYtNlX4eppfrMjN'
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
