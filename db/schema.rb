dee49774cdce59c186fd670d377db335ffdf9dfe
sidongithub <koolspy.siddhu@gmail.com>
2015-01-21 18:23:32 +0530

    tokenand active to users migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120120049) do
+ActiveRecord::Schema.define(version: 20150121125218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 20150120120049) do
     t.datetime "updated_at"
     t.integer  "role"
     t.string   "postalcode"
+    t.string   "token"
+    t.integer  "active"
   end
 
 end
